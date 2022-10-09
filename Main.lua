b71b854d555a0b3d53c9324582213fefac3799bb
BilzaDev <110744777+BilzaDev@users.noreply.github.com>
2022-10-09 18:09:37 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -89,7 +89,7 @@ if game.PlaceId == 155615604 then
         Event:InvokeServer(A_1)
 	end)
 
-    Section1:NewDropdown("Gun Mod", "Makes the gun op", {"M9", "Remington 870", "AK-47", "Tazer"}, function(weapon)
+    Section1:NewDropdown("Gun Mod", "Makes the gun op", {"M9", "Remington 870", "AK-47"}, function(weapon)
         local module = nil
         if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild(weapon) then
             module = require(game:GetService("Players").LocalPlayer.Backpack[weapon].GunStates)
@@ -100,7 +100,7 @@ if game.PlaceId == 155615604 then
             module["MaxAmmo"] = 100000000
             module["CurrentAmmo"] = 100000000
             module["StoredAmmo"] = 100000000
-            module["FireRate"] = 0.3
+            module["FireRate"] = 0.09
             module["Spread"] = 0
             module["Range"] = math.huge
             module["Bullets"] = 10
@@ -109,7 +109,7 @@ if game.PlaceId == 155615604 then
         end
     end)
 
-	enabled = false
+	enabled = false 
 	Section1:NewButton("EnableESP", "ESP", function()
 		if enabled == false then
 			enabled = true
@@ -127,6 +127,11 @@ if game.PlaceId == 155615604 then
 		end
 	end)
 
+	Section1:NewTextBox("Kick A Player", "Kick a player", function(txt)
+		playerlmao = game.Players:FindFirstChild(txt)
+		playerlmao:Kick()
+	end)
+
 	Section1:NewButton("Destroy all Doors", "DestroyDoors", function()
 		game.Workspace.Doors:Destroy()
 	end)
@@ -138,6 +143,7 @@ if game.PlaceId == 155615604 then
 	active = false
 	Section1:NewButton("GodMode", "GodMode", function()
 		game.Players.LocalPlayer.Character.Humanoid.MaxHealth = math.huge
+		game.Players.LocalPlayer.Character.Humanoid.Health = 10000000
 	end)
 
 	--Player things
@@ -150,11 +156,134 @@ if game.PlaceId == 155615604 then
 	Section2:NewSlider("Jumppower", "Changes your jump power to a set amount.", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
     	game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
 	end)
-else
+
+elseif game.PlaceId == 7952502098 then
 	game.StarterGui:SetCore("SendNotification", {
-	Title = "This game is not supported!";
-	Text = "This game is not supported with CatHub. Try another script!";
+	Title = "Thank you for using CatHub.";
+	Text = "loading Glass Bridge UI...";
 	Duration = 3;
 	})
+	wait(3)
+	game.StarterGui:SetCore("SendNotification", {
+	Title = "Loaded!";
+	Text = "The Glass Bridge UI has loaded!";
+	Duration = 1;
+	})
+	wait(5)
+	local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+	local Window = Library.CreateLib("Glass Bridge [BETA]", "Sentinel")
+	local Main = Window:NewTab("Main")	
+	local Section1 = Main:NewSection("Main")
 
+	Section1:NewButton("TP To End", "TP To End", function()
+		winspot = game.Workspace["Glass Bridge"].Finish["Money Pig"].MeshPart.Position
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(winspot)
+	end)
+
+elseif game.PlaceId == 2399948276 then
+	game.StarterGui:SetCore("SendNotification", {
+	Title = "Thank you for using CatHub.";
+	Text = "loading Hyper Realistic Guns UI...";
+	Duration = 3;
+	})
+	wait(3)
+	game.StarterGui:SetCore("SendNotification", {
+	Title = "Loaded!";
+	Text = "The Hyper Realistic Guns UI has loaded!";
+	Duration = 1;
+	})
+	wait(5)
+	local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+	local Window = Library.CreateLib("Hyper Realistic Guns [BETA]", "Sentinel")
+	local Main = Window:NewTab("Main")	
+	local Section1 = Main:NewSection("Main")
+
+
+		local Settings = {
+		['Material'] = Enum.Material.Neon, -- Material
+		['Color'] = Color3.fromRGB(50, 164, 168), -- Color
+		['Transparency'] = 0.7 -- 0 to 1 Transparency
+	}
+
+	local ScreenGui = Instance.new('ScreenGui', game.CoreGui) -- Create screengui
+	ScreenGui.IgnoreGuiInset = true
+
+	local ViewportFrame = Instance.new('ViewportFrame', ScreenGui) -- Create viewport and define properties
+	ViewportFrame.CurrentCamera = workspace.CurrentCamera
+	ViewportFrame.Size = UDim2.new(1,0,1,0)
+	ViewportFrame.BackgroundTransparency = 1
+	ViewportFrame.ImageTransparency = Settings.Transparency
+
+	local Chasms = {} -- Array for storing parts
+
+	function generateChasm(player) -- functions that generates chasms for the player specififed
+		local Character = workspace:FindFirstChild(player.Name)
+		
+		if Character then
+			for _,Part in pairs(Character:GetChildren()) do
+				if Part:IsA('Part') or Part:IsA('MeshPart') then
+					local Chasm = Part:Clone()
+					
+					for _,Child in pairs(Chasm:GetChildren()) do
+						if Child:IsA('Decal') then
+							Child:Destroy()
+						end
+					end
+					
+					Chasm.Parent = ViewportFrame
+					Chasm.Material = Settings.Material
+					Chasm.Color = Settings.Color
+					Chasm.Anchored = true
+					
+					table.insert(Chasms, Chasm)
+				end
+			end
+		end
+	end
+
+	function clearChasms() -- remove all chasms
+		for _,Chasm in pairs(Chasms) do
+			Chasm:Destroy()
+		end
+		
+		Chasms = {}
+	end
+
+	Section1:NewButton("Give Stats [LOCAL]", "Give Stats [LOCAL]", function()
+		game.Players.LocalPlayer.leaderstats.Kills.Value += 10000
+	end)
+
+	-- Script generated by SimpleSpy - credits to exx#9394
+	Section1:NewDropdown("Change Team", "Give Stats [LOCAL]", {"Blue Team", "Red Team"}, function(option)
+		if option == "Blue Team" then
+			local args = {
+				[1] = BrickColor.new(1011)
+			}
+			game:GetService("ReplicatedStorage").RequestTeam:FireServer(unpack(args))
+		elseif option == "Red Team" then
+			local args = {
+				[1] = BrickColor.new(332)
+			}
+			game:GetService("ReplicatedStorage").RequestTeam:FireServer(unpack(args))
+		end
+	end)
+
+	enabled2 = false 
+	Section1:NewButton("Enable ESP", "ESP", function()
+		if enabled2 == false then
+			enabled2 = true
+			while game:GetService('RunService').RenderStepped:Wait() do -- loop this process
+				clearChasms()
+				
+				for _,Player in pairs(game:GetService('Players'):GetPlayers()) do
+					if Player ~= game:GetService('Players').LocalPlayer then
+						generateChasm(Player)
+					end
+				end
+			end
+		elseif enabled2 == true then
+			print("Esp Already activated")
+		end
+	end)
+			
 end
